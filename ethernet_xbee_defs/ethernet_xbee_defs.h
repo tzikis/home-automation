a1b16cbd96554623df86245335cf50b6fdda353d
Vasileios Georgitzikis <georgitzik@ceid.upatras.gr>
2011-03-15 00:54:08 +0200

    Added the 'library' with the message definitions

---
+++ ethernet_xbee_defs/ethernet_xbee_defs.h
@@ -0,0 +1,20 @@
+#define STATE_OFF 32
+#define STATE_ON 33
+#define STATE_AUTO_ON 34
+#define STATE_AUTO_OFF 35
+
+#define OPEN 36
+#define CLOSE 37
+#define AUTO 38
+
+#define ENABLE_PER 39
+#define ENABLE_EVT 40
+
+#define DISABLE_PER 41
+#define DISABLE_EVT 42
+
+#define STATUS 43
+
+#define HALT 44
+
+#define STARTING 45
